60c0c6129a472282e866c3c168683fce431ee6df
Salman <mohammadalfarisi00@gmail.com>
2021-08-31 19:46:57 +0700

    index.ts fix + changed command prefix to git add .

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,38 @@
-import { Client, Message } from 'discord.js'
-import Discord from 'discord.js'
-import secret from '../secrets/bot.json'
-import config from '../config.json'
-import allCommands from './allCommands'
+import { Client, Message, Intents } from 'discord.js';
+import Discord from 'discord.js';
+import secret from '../secrets/bot.json';
+import config from '../config.json';
+import allCommands from './allCommands';
 
-const client: Client = new Discord.Client()
+const intents: Intents = new Discord.Intents(32767);
 
-client.on('message', (message: Message) => {
-    try {
-        if (
-            message.content.startsWith(config.commandPrefix) &&
-            !message.author.bot
-        ) {
-            const args = message.content
-                .slice(config.commandPrefix.length)
-                .split(/ +/)
-                .filter((element) => element)
-            const commandString = args.shift()
-            if (commandString) {
-                const command = allCommands.get(commandString)
-                if (command && !command.disabled) {
-                    command.action(client, message, args)
-                }
-            }
-        }
-    } catch (e) {
-        console.log(e)
-    }
-})
+const client: Client = new Discord.Client({ intents });
+
+client.on('messageCreate', (message: Message) => {
+	try {
+		if (
+			message.content.startsWith(config.commandPrefix) &&
+			!message.author.bot
+		) {
+			const args = message.content
+				.slice(config.commandPrefix.length)
+				.split(/ +/)
+				.filter((element) => element);
+			const commandString = args.shift();
+			if (commandString) {
+				const command = allCommands.get(commandString);
+				if (command && !command.disabled) {
+					command.action(client, message, args);
+				}
+			}
+		}
+	} catch (e) {
+		console.log(e);
+	}
+});
 
 client.on('ready', () => {
-    console.log('Discord bot ready')
-})
+	console.log('Discord bot ready');
+});
 
-client.login(secret.token)
+client.login(secret.token);
